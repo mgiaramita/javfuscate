87322b873c5caf710360571a12fea613edd7245b
Mathew Giaramita <m.giaramita@gmail.com>
2017-02-23 06:55:12 -0500

    1

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-class HelloWorld{
-	public static void main(String[] args){
-		System.out.println("Hello World");
-		int loop = 3;
-		for(int i = 0; i < loop; i++){
-			System.out.println(i);
-		}
-	}
-}
