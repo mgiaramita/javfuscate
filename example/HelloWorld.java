793648c87696fbcc2fd97071f77e29c0b9a77b2a
Mathew Giaramita <m.giaramita@gmail.com>
2017-02-23 06:53:47 -0500

    Original Program

---
+++ example/HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		int loop = 3;
+		for(int i = 0; i < loop; i++){
+			System.out.println(i);
+		}
+	}
+}
